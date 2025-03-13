7f6608076ac00f42b5677570d8a2f275a8d937b8
Nwosu Chukwuemeka Darlington <fritzdultimate@gmail.com>
2025-03-13 11:37:13 +0100

    v

---
+++ resources/views/emails/layouts/footer.blade.php
@@ -0,0 +1,12 @@
+                <tr>
+                    <td style="padding:0 44px;font-size:1rem">
+                        <p style="font-weight:300;line-height:30px;font-size:1rem">
+                            If you didn't initiate this transaction, please contact our support team
+                            immediately via in-app or email <span class="m_-6104106351755992953support"> <a href="mailto:support@grey.co" target="_blank">support@grey.co</a> </span>
+                        </p>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    </body>
+</html>
---
+++ resources/views/emails/layouts/header.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+    <body>
+        <div style="max-width:800px;margin:0 auto;border:0.336601px solid rgba(19,18,18,0.1);border-radius:8px">
+            <table>
+                <tbody style="font-size:10px;color:#1a1a1a;font-weight:500">
+                    <tr style="float:left;width:100%;padding:40px 44px">
+                        <td>
+                            <img src="https://ci3.googleusercontent.com/meips/ADKq_NYSXtYT8FrjoxUgbQqnD62RTn7dohUBU3cvOLPsD1DWkycR67F4BkNbVLpw6vtfM4iqcIt426KsgA1Ad2jaxFvuBT18mYWuTSy1Iqje0fbapM5YXRTY2vkRlTTd-1eFAydoQVD-g1WFsT52WVo509nBl-JGTA=s0-d-e1-ft#https://res.cloudinary.com/abokiafrica/image/upload/v1642066376/mail%20template/logo_jrydtr.png" alt="Grey" style="width:120px;height:35px">
+                        </td>
+                    </tr>
+        
+                    <tr style="float:left;width:100%;padding:40px 44px">
+                        <td>
+                            <img src="https://ci3.googleusercontent.com/meips/ADKq_Nans0tnJJs6hl3_Nk4-R2phN9PgFWJgIIek9JgUoxj-Ky6ZhdKY5oofDm123VIlyY-Uz7pX_Ki-0ZVBK5MI-DyWUPVmOwG6xqQAOUREWOEN6BHFOAE93I0zSSVnLXj6yKJkspTQpiorRmP6DiefdDSmpTxw=s0-d-e1-ft#https://res.cloudinary.com/abokiafrica/image/upload/v1676651101/mail%20template/logo_white.png" alt="Grey" style="width:120px;height:35px">
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
