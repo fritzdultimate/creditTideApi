b497306e76bc890f51b038fcac01cdcb41362d30
Nwosu Chukwuemeka Darlington <fritzdultimate@gmail.com>
2026-05-14 10:31:12 +0000

    v

--- config/sanctum.php
+++ config/sanctum.php
@@ -15,11 +15,16 @@ return [
     |
     */
 
-    'stateful' => explode(',', env('SANCTUM_STATEFUL_DOMAINS', sprintf(
-        '%s%s',
-        'localhost,localhost:3000,127.0.0.1,127.0.0.1:8000,credittidestockcompany.com,credittidestockcompany.com,api.credittidestockcompany.com,::1','credittidestockcompany.com','api.credittidestockcompany.com',
-        Sanctum::currentApplicationUrlWithPort()
-    ))),
+    // 'stateful' => explode(',', env('SANCTUM_STATEFUL_DOMAINS', sprintf(
+    //     '%s%s',
+    //     'localhost,localhost:3000,127.0.0.1,127.0.0.1:8000,credittidestockcompany.com,credittidestockcompany.com,api.credittidestockcompany.com,::1','credittidestockcompany.com','api.credittidestockcompany.com',
+    //     Sanctum::currentApplicationUrlWithPort()
+    // ))),
+
+    'stateful' => explode(',', env(
+        'SANCTUM_STATEFUL_DOMAINS',
+        'localhost,localhost:3000,127.0.0.1,127.0.0.1:8000,::1,credittidestockcompany.com,api.credittidestockcompany.com'
+    )),
 
     /*
     |--------------------------------------------------------------------------
